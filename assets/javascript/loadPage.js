604587c0831425cba525d9009dc443322d9f7347
GabrielCabral352 <gabrielcabral352@gmail.com>
2026-03-11 12:01:42 -0300

    bug fixes and about me section

--- assets/javascript/loadPage.js
+++ assets/javascript/loadPage.js
@@ -3,6 +3,7 @@ jQuery.ajaxSetup({ async: false });
 $.get("./pages/header.html", '', function (data) { $("#body_section").append(data); });
 $.get("./pages/home.html", '', function (data) { $("#content").append(data); });
 jQuery.ajaxSetup({ async: true });
+
 const pages = [
     "./pages/home.html", 
     "./pages/about.html", 
@@ -13,15 +14,18 @@ const pages = [
     "./pages/contact.html"
 ];
 
-function rebuildPage(){
+function rebuildPage() {
     document.querySelectorAll(".__nav_item").forEach(function (item, idx) {
-        item.addEventListener('click', function () {
-            document.getElementById("content").innerHTML = ""
-            jQuery.ajaxSetup({ async: false });
-            $.get(pages[idx], '', function (data) { $("#content").append(data); });
-            jQuery.ajaxSetup({ async: true });
+        item.addEventListener("click", function () {
+
+            $("#content").fadeOut(100, function () {
+                $("#content").load(pages[idx], function () {
+                    $("#content").fadeIn(100);
+                });
+            });
+
         });
-    }); 
+    });
 }
 
 
